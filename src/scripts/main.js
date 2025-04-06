10998eb6d643be9e481d087275dd93eecd29b3b0
RyanAthaide <athaideryan1@gmail.com>
2025-04-06 12:12:08 -0300

    criando arquivo inscrever.html dentro de pages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 import '../styles/input.css';
+
+// tirando recarregamento do form
+document.querySelector('form button[type="submit"]').addEventListener('click', function(e) {
+    // Impede qualquer comportamento padrão (tipo submit)
+    e.preventDefault();
+  
+  });
+  
