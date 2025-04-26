19de011fd75f6f6811d0f8822cb4cab8e62416f9
RyanAthaide <athaideryan1@gmail.com>
2025-04-26 15:47:24 -0300

    feat: Criando função para redirecionamento do botão 'cadastre-se' para a pagina cadastroForm.html

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,18 @@
 
+// Redirecionamento e retorno para paginas de login e cadastro com b0tões 'entrar' e 'cadastre-se'
+const btnCadastro = document.getElementById("btn-cadastro");
+
+btnCadastro.addEventListener('click', function() {
+    window.location.href = "/pages/cadastroForm.html";
+});
+
+
 // entrando na pagina increva-se com e-mail
 const inscrever = document.getElementById("inscricao");
 inscrever.addEventListener('click', function() {
     window.location.href = "/pages/inscrever.html";
 });
+
 
 // Prevenção de recarregamento do form
 const formButton = document.querySelector('form button[type="submit"]');
