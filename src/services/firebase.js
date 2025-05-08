e13a5ce12e86fa0137281984e2401b56e23a29d5
RyanAthaide <athaideryan1@gmail.com>
2025-05-08 02:11:22 -0300

    instalando pacote do firebase e importando

---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+ 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxYgnOqHCClRYRGTQ9QmA9XnaLLJU3guM",
+  authDomain: "nutrifit-auth-3872c.firebaseapp.com",
+  projectId: "nutrifit-auth-3872c",
+  storageBucket: "nutrifit-auth-3872c.firebasestorage.app",
+  messagingSenderId: "578333420202",
+  appId: "1:578333420202:web:38cbef9d5b0c349ebb3ed3",
+  measurementId: "G-H6N2RJX4TL"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
