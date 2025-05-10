306ddeb51e9a687d757d85e8d06bbcd50692adc7
RyanAthaide <athaideryan1@gmail.com>
2025-05-09 22:47:19 -0300

    feat: adiciona estrutura de páginas JS - cria pasta 'pages' dentro de 'scripts' - Move arquivos inscricao.js, cadastro.js e cria arquivo dashboard.js - prepara estrutura básica para páginas do site

---
+++ src/scripts/pages/cadastro.js
@@ -0,0 +1,7 @@
+
+// Redirecionamento e retorno para paginas de login e cadastro com botão 'entrar'.
+const btnEntrar = document.getElementById("btn-entrar");
+
+btnEntrar.addEventListener('click', function() {
+    window.location.href = "index.html";
+});    
