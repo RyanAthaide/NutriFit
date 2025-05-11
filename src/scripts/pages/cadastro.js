31aeddaed23760e437f973bbc436ee499186d3bc
RyanAthaide <athaideryan1@gmail.com>
2025-05-11 20:16:13 -0300

    feat: Fazendo redirecionamento e retorno para a pagina de login com botão 'entrar' na página de cadastro.

--- src/scripts/pages/cadastro.js
+++ src/scripts/pages/cadastro.js
@@ -1,7 +1,6 @@
 
 // Redirecionamento e retorno para paginas de login e cadastro com botão 'entrar'.
 const btnEntrar = document.getElementById("btn-entrar");
-
 btnEntrar.addEventListener('click', function() {
     window.location.href = "index.html";
 });    
