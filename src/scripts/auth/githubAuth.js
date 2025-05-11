57277ed5795140083ced10dc85ac93f8cdeb8125
RyanAthaide <athaideryan1@gmail.com>
2025-05-11 10:14:28 -0300

    feat: implementa autenticação com Github via Firebase
    - Adiciona função githubLogin() no módulo de autenticação
    - Utiliza signInWithPopup do Firebase Auth
    - Redireciona para /pages/dashboard.html após login bem-sucedido
    - Trata erros com mensagens no console
    - Configura GithubAuthProvider como provedor de autenticação

---
+++ src/scripts/auth/githubAuth.js
@@ -0,0 +1,16 @@
+// import para autenticacao via github
+import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../../services/firebase.js";
+
+export async function githubLogin(params) {
+    const provider = new GithubAuthProvider();
+
+    try {
+        const result = await signInWithPopup(auth, provider);
+        console.log("Usuário logado: ", result.user);
+        window.location.href = "./public/pages/dashboard.html";
+    }  
+    catch (error) {
+        console.error("Erro ao fazer login:", error.massage);
+    }
+}
