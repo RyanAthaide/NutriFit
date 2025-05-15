f3b71e2797fe6376eee2cda9162d0f9d17c9ad81
RyanAthaide <athaideryan1@gmail.com>
2025-05-15 01:01:49 -0300

    chore(firebase): configura acesso ao Firestore
    - Exporta instância 'db' do Firestore
    - Utiliza a instância 'app' do Firebase
    - Prepara para operações de banco de dados

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBxYgnOqHCClRYRGTQ9QmA9XnaLLJU3guM",
@@ -14,5 +15,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 const analytics = getAnalytics(app);
