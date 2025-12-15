a898ed14541ff79178562a6928d20065bd36c2c1
RyanAthaide <athaideryan1@gmail.com>
2025-12-15 16:43:10 -0300

    fix: configuração correta do Firebase para Vite e Vercel

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,14 +5,15 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxYgnOqHCClRYRGTQ9QmA9XnaLLJU3guM",
-  authDomain: "nutrifit-auth-3872c.firebaseapp.com",
-  projectId: "nutrifit-auth-3872c",
-  storageBucket: "nutrifit-auth-3872c.appspot.com",
-  messagingSenderId: "578333420202",
-  appId: "1:578333420202:web:38cbef9d5b0c349ebb3ed3",
-  measurementId: "G-H6N2RJX4TL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
